e90d9ac569b1ce619a9f65c23086793bd1d92214
Mac <Anton-r@live.com>
2020-11-18 16:42:48 +0500

    hh

--- python_adv/thred_work.py
+++ python_adv/thred_work.py
@@ -2,16 +2,25 @@ import threading
 
 
 def one():
-    print("hello world")
+    for _ in range(50):
+        print("hello world", end=" ")
 
 def two():
-    for i in range(10):
+    for _ in range(50):
+        print("-------   ", end=" ")
+
+def three():
+    for _ in range(50):
         print("other world", end=" ")
 
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=one)
     t2 = threading.Thread(target=two)
-    
+    t3 = threading.Thread(target=three)
+
+    # will take kare about race com=ndition and lock by it self c
     t1.start()
     t2.start()
+    t3.start()
+
