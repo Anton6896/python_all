73de27ce8fa78e3fab2783c84f776197f7f319b6
Mac <Anton-r@live.com>
2020-11-18 16:47:37 +0500

    gg

---
+++ python_adv/sinc_thred.py
@@ -0,0 +1,9 @@
+import threading
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
--- python_adv/thred_work.py
+++ python_adv/thred_work.py
@@ -2,16 +2,20 @@ import threading
 
 
 def one():
+    print('\n')
     for _ in range(50):
         print("hello world", end=" ")
+    print('\n')
 
 def two():
     for _ in range(50):
         print("-------   ", end=" ")
+    print('\n')
 
 def three():
     for _ in range(50):
         print("other world", end=" ")
+    print('\n')
 
 
 if __name__ == "__main__":
@@ -19,8 +23,9 @@ if __name__ == "__main__":
     t2 = threading.Thread(target=two)
     t3 = threading.Thread(target=three)
 
-    # will take kare about race com=ndition and lock by it self c
+
     t1.start()
+    t1.join()  # whait toll t1 finish 
     t2.start()
     t3.start()
 
