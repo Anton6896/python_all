fabb2a77c421092fcdbe503af8863fefa799129c
Mac <Anton-r@live.com>
2020-12-06 21:26:15 +0500

    ff

--- dynamic_prog/grid_travel.py
+++ dynamic_prog/grid_travel.py
@@ -1,9 +1,36 @@
 
-def gt():
+def gt(r, c) -> int:
     # count the ways to travel in griad (top left -> down right)
-    
-    pass
+    # the regular recursio -> time O(2^(n+m))
+    if r == 1 and c == 1:
+        return 1
+    elif r == 0 or c == 0:
+        return 0
+
+    # count the sum of choises
+    return gt(r-1, c) + gt(r, c-1)
+
+
+def gt_plus(r, c, memo={}) -> int:
+    # time -> O(n*m)
+    key_1 = str(r)+','+str(c)
+    if key_1 in memo:
+        return memo[key_1]
+
+    if r == 1 and c == 1:
+        return 1
+    elif r == 0 or c == 0:
+        return 0
+
+    memo[key_1] = gt_plus(r-1, c, memo) + gt_plus(r, c-1, memo)
+    return memo[key_1]
 
 
 if __name__ == "__main__":
-    pass
+    # print(f'the regular tester {gt(3,3)}')
+    # print(f'the regular tester {gt(2,2)}')
+    # print(f'the regular tester {gt(1,1)}')
+
+    print(f'the memo way tester {gt_plus(2,2)}')
+    print(f'the memo way tester {gt_plus(5,4)}')
+    print(f'the memo way tester {gt_plus(18,18)}')
--- dynamic_prog/memoization.py
+++ dynamic_prog/memoization.py
@@ -1,11 +1,14 @@
 
 def fib(n: int) -> int:
+    #! the regular approach 
     if n <= 2:
         return 1
     return fib(n-1) + fib(n-2)
 
 
 def fib_2(n: int, memo=dict()) -> int:
+    #* this way to do tree lookup 
+    #* by using memoization decreasing the time complexity
     if n in memo:  # look for keys
         return memo[n]
 
