0466d1518bb46974a92c38313a5c1b61935470db
anton <anton-r@live.com>
2020-12-08 09:48:08 +0500

    ss

---
+++ dynamic_prog/memo_CanSum.py
@@ -0,0 +1,11 @@
+
+
+def one(arr: list, num: int) -> bool:
+    # if it passible to generate some number by using an given array
+    
+
+    return False
+
+
+if __name__ == "__main__":
+    one([1, 3, 4, 7], 7)
