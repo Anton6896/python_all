da3657c84c252fb63af7d3ab71f91d3323853783
Ant_win <anton-r@live.com>
2020-12-08 15:10:58 +0500

    ff

--- dynamic_prog/memo_CanSum.py
+++ dynamic_prog/memo_CanSum.py
@@ -1,5 +1,8 @@
 
 
+from typing import NoReturn
+
+
 def one(arr: list, target: int) -> bool:  # brute force
     # if it passible to generate some number
     # by using an given array
@@ -18,7 +21,7 @@ def one(arr: list, target: int) -> bool:  # brute force
     return False
 
 
-def mem_one(arr: list, target: int, memo={}) -> bool:  # memozing
+def mem_one(arr: list, target: int, memo: dict) -> bool:  # memozing
     # O(arr*target) -> time
     # O(target) -> space
     if target in memo:
@@ -39,7 +42,8 @@ def mem_one(arr: list, target: int, memo={}) -> bool:  # memozing
 
 
 def get_options(target: int, arr: list) -> list:
-    # time : O(target^arr * arr)
+    # time : O(arr^target * target)
+    # size : O(target^2)
     if target == 0:
         return []
     elif target < 0:
@@ -56,7 +60,8 @@ def get_options(target: int, arr: list) -> list:
     return None
 
 
-def get_options_memo(target: int, arr: list, memo=dict) -> list:
+def get_options_memo(target: int, arr: list, memo: dict) -> list or None:
+    # time O(target^2 * arr)
     if target in memo:
         return memo[target]
     elif target == 0:
@@ -77,17 +82,53 @@ def get_options_memo(target: int, arr: list, memo=dict) -> list:
     return None
 
 
+def bestSum(target: int, arr: list) -> list:
+    # get the smallest arr that in sum return target
+    if target == 0:
+        return []
+    elif target < 0:
+        return None
+
+    shortestCombination = None
+
+    for num in arr:
+        target_r = target - num
+
+        option = bestSum(target_r, arr)
+        if option != None: # option is list
+            option.append(num)
+            if shortestCombination == None or len(option) < len(shortestCombination):
+                shortestCombination = option
+    
+    return shortestCombination
+
+
+def bestSum_memo(target: int, arr: list, memo: dict) -> list:
+    # get the smallest arr that in sum return target
+    pass
+
+
 if __name__ == "__main__":
     # print(f"find sum : {one([2, 4], 7)}")  # false
     # print(f"find sum : {one([5, 3, 4, 7], 7)}")  # true
     # print(f"find sum : {one([7, 14], 300)}") # False (too match time)
-    print(f"find sum : {mem_one([7, 14], 301, {})}")  # true
-    print(f"find sum : {mem_one([5, 3, 4, 7], 7, {})}")  # true
-    print()
-    print(f'arr : {get_options(7, [2,3])}')  # ok
-    print(f'arr : {get_options(7, [5,3,4,7])}')  # ok
-    print(f'arr : {get_options(7, [2,4])}')  # None
-    print()
-    print(f'mem arr : {get_options_memo(7, [5,3,4,7], {})}')
-    print(f'mem arr : {get_options_memo(7, [2,3], {})}')
-    print(f'mem arr : {get_options_memo(301, [7,14], {})}')
+    # print(f"find sum : {mem_one([7, 14], 301, {})}")  # true
+    # print(f"find sum : {mem_one([5, 3, 4, 7], 7, {})}")  # true
+    # print()
+    # print(f'arr : {get_options(7, [2,3])}')  # ok
+    # print(f'arr : {get_options(7, [5,3,4,7])}')  # ok
+    # print(f'arr : {get_options(7, [2,4])}')  # None
+    # print()
+    # print(f'mem arr : {get_options_memo(7, [5,3,4,7], {})}')
+    # print(f'mem arr : {get_options_memo(7, [2,3], {})}')
+    # print(f'mem arr : {get_options_memo(301, [7,14], {})}')
+    print(f"\nbest sum : {bestSum(7,[5,3,4,7])}")
+    print(f"best sum : {bestSum(8,[2,3,5])}")
+    print(f"best sum : {bestSum(8,[1,4,5])}")
+
+    print(f"\nbest sum memo: {bestSum_memo(7,[5,3,4,7],{})}")
+    print(f"best sum memo: {bestSum_memo(8,[2,3,5],{})}")
+    print(f"best sum memo: {bestSum_memo(8,[1,4,5],{})}")
+    print(f"best sum memo: {bestSum_memo(100,[1,2,5,25],{})}")
+
+# requirements.txt
