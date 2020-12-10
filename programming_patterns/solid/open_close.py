b3161bdc0a117f70339de3c4f4b09cbaf8e26048
Mac <Anton-r@live.com>
2020-12-10 12:52:56 +0500

    products cretion

--- programming_patterns/solid/open_close.py
+++ programming_patterns/solid/open_close.py
@@ -6,4 +6,53 @@ solid design principles (Robert C Martin)
 4.
 5.
 """
+# ocp -> open for extention close for modification
+from enum import Enum
 
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+
+
+class Product:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.size = size
+        self.color = color
+
+
+class Specifications:
+    # Base class
+    def is_satisfied(self, item: Product):
+        pass
+
+
+class Filter:
+    # Base class
+    def filter(self, item: Product, spec: Specifications):
+        pass
+
+
+class BetterFilter(Filter):
+    def filter(self, items: Product, spec: Specifications) -> Product:
+        for item in items:
+            if spec.is_satisfied(item):
+                yield item
+
+
+if __name__ == "__main__":
+    apple = Product('Apple', Color.GREEN, Size.SMALL)
+    tree = Product('Tree', Color.GREEN, Size.LARGE)
+    house = Product('House', Color.RED, Size.LARGE)
+
+    my_products = [
+        apple, tree, house
+    ]
