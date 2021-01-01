ca4539817d833030f0b80053e3dd66664a08500c
ant_pc <anton-r@live.com>
2021-01-01 19:58:58 +0500

    d

--- programming_patterns/solid/dip.py
+++ programming_patterns/solid/dip.py
@@ -26,19 +26,45 @@ class Person:
         self.name = name
 
 
-class RelationBroser:
+class RelationBroser:  # an interface
     @abstractclassmethod
     def find_all_children_of(self, name): pass
 
 
-class Relationship(Relation):
-    # ==================   low level class
-    # may use db etc
+class Relationship(RelationBroser):  # low level
 
-    relations = []
+    relations = []  # db
 
-    def add_parrent_and_child(self, parent, child):
+    # db manipulation
+    def add_parrent_and_child(self, parent: Person, child: Person) -> None:
         self.relations.append((parent, Relation.PARENT, child))
         self.relations.append((child, Relation.PARENT, parent))
-    
-    
+
+    def find_all_children_of(self, name):
+        for r in self.relations:
+            if r[0].name == name and r[1] == Relation.PARENT:
+                yield r[2].name
+
+# ==========================================
+
+
+class Research:  # height level class
+    # for now this class is independent from low level implementation
+    def __init__(self, browser: Relationship):
+        for p in browser.find_all_children_of('John'):
+            print(f'John has a child called {p}')
+
+
+if __name__ == "__main__":
+
+    # low level for testing
+    parent = Person('John')
+    child1 = Person('mark')
+    child2 = Person('judy')
+
+    relay = Relationship()  
+    relay.add_parrent_and_child(parent, child1) 
+    relay.add_parrent_and_child(parent, child2)  
+
+    #  user testing
+    Research(relay)
