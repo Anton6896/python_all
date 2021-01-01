76293489429bffef69444bc6d596b2bddc8c4594
ant_pc <anton-r@live.com>
2021-01-01 19:25:52 +0500

    d

--- programming_patterns/solid/dip.py
+++ programming_patterns/solid/dip.py
@@ -8,4 +8,37 @@ solid design principles (Robert C Martin)
         ( use couple interfaces ech interface for his neeads (user neeads), 
         that user no need to implement more then they need to )
 5. DIP -> dependency inversion 
+        ( point is to release dependency from low level )
 """
+
+from abc import abstractclassmethod
+from enum import Enum
+
+
+class Relation(Enum):
+    PARENT = 0
+    CHILD = 1
+    SIBLIN = 2
+
+
+class Person:
+    def __init__(self, name) -> None:
+        self.name = name
+
+
+class RelationBroser:
+    @abstractclassmethod
+    def find_all_children_of(self, name): pass
+
+
+class Relationship(Relation):
+    # ==================   low level class
+    # may use db etc
+
+    relations = []
+
+    def add_parrent_and_child(self, parent, child):
+        self.relations.append((parent, Relation.PARENT, child))
+        self.relations.append((child, Relation.PARENT, parent))
+    
+    
