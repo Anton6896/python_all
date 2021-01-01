9cc6f5bed86fe06687f6d6d0e426e774b724718a
Ant_win <anton-r@live.com>
2021-01-01 18:40:46 +0500

    ff

---
+++ programming_patterns/solid/dip.py
@@ -0,0 +1,11 @@
+"""
+solid design principles (Robert C Martin)
+1. srp -> singel responsibility for object
+2. ocp -> open close principeles
+3. LSP ->   Liskov substitution principle 
+        (the inherit class must not violate the parent class behavior )
+4. ISP -> interface segregation principle 
+        ( use couple interfaces ech interface for his neeads (user neeads), 
+        that user no need to implement more then they need to )
+5. DIP -> dependency inversion 
+"""
