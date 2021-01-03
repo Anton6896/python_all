0e63b283d95d5dc8856000ff1f1e16f503125603
anton <anton-r@live.com>
2021-01-03 12:42:25 +0500

    dd

--- programming_patterns/builder/builder.py
+++ programming_patterns/builder/builder.py
@@ -26,17 +26,19 @@ class HtmlElement:
 
     def __str(self, indent):
         lines = []
-        i = " " * ((indent+1) * self.indent_size)
+        i = " " * ((indent+1) * self.indent_size)  # i = 4 -> "    "
         lines.append(f"{i}<{self.name}>")
 
         if self.text:
-            i1 = " "*((indent+1)*self.indent_size)
+            i1 = " "*((indent+2)*self.indent_size)  # i1 = 8 -> "    "
             lines.append(f"{i1}{self.text}")
 
+        # recursion call for all elements from HtmlBuilder
         for e in self.elements:
             lines.append(e.__str(indent+1))
 
         lines.append(f"{i}</{self.name}>")
+
         return "\n".join(lines)
 
     def __str__(self):
@@ -48,8 +50,48 @@ class HtmlElement:
 
 
 class HtmlBuilder:
-    pass
+    __root = HtmlElement()
+
+    def __init__(self, root_name):
+        self.root_name = root_name
+        self.__root.name = root_name
+
+    # not fluent implementation
+    def add_child(self, ch_name, ch_text):
+        self.__root.elements.append(
+            HtmlElement(ch_name, ch_text)
+        )
+
+    # fluent implementation
+    def add_child_fluent(self, ch_name, ch_text):
+        self.__root.elements.append(
+            HtmlElement(ch_name, ch_text)
+        )
+        return self
+
+    def __str__(self):
+        return str(self.__root)
+
+    def clear(self):
+        self.__root = HtmlElement(name=self.root_name)
 
 
 if __name__ == "__main__":
-    func_1()
+    # func_1()
+
+    # ordinary call 
+    builder = HtmlBuilder('ul')
+    builder = HtmlElement.create('ul')
+    builder.add_child('li', 'text 1')
+    builder.add_child('li', 'text 2')
+
+    print(f'ordinary builder \n{builder}')
+
+
+    # fluent call 
+    print()
+    builder.clear()
+    builder.add_child_fluent('li', 'hello') \
+        .add_child_fluent('li', 'world')
+    print('Fluent builder:')
+    print(builder)
