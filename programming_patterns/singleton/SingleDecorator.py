5cd37ee3fe253039e76617a7b4641d38fc217ca7
Mac <Anton-r@live.com>
2021-01-14 16:37:38 +0500

    ff

--- programming_patterns/singleton/SingleDecorator.py
+++ programming_patterns/singleton/SingleDecorator.py
@@ -23,8 +23,30 @@ class DataClass:
         print("using data base ")
 
 
+"""
+oder way is to use an Meta class 
+"""
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class DataClass_2(metaclass=Singleton):
+    def __init__(self):
+        print("class loading")
+
+
 if __name__ == '__main__':
     d1 = DataClass()
     d2 = DataClass()  # <- init call only once ! for this class
+    print(d1 is d2, "\n")
 
-    print(d1 is d2)
+    d3 = DataClass_2()
+    d4 = DataClass_2()
+    print(d3 is d4)
