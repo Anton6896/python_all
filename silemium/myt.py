a1014fe325e740a59393f88db970ad32c83b060b
Ant <fedora@mail.com>
2021-01-17 14:10:11 +0500

    ff

--- silemium/myt.py
+++ silemium/myt.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 import os
 import time
 
@@ -22,7 +27,6 @@ def mac_tes():
     # main = search.find_element_by_class_name("LC20lb DKV0Md")
     main = search.find_element_by_class_name("tF2Cxc")
 
-
     # wright data to file
     # with open(str(os.getcwd()) + "/test.txt", "a+") as f:
     #     f.write(driver.page_source)
@@ -31,5 +35,56 @@ def mac_tes():
     driver.quit()
 
 
+def linux_tes():
+    """
+    look for some blog entries 
+
+    xpath :: https://www.guru99.com/xpath-selenium.html
+    with xpath is more comfartable to make an search in page 
+
+    """
+
+    path_lin = "/usr/local/bin/chromedriver"
+    driver = webdriver.Chrome(path_lin)
+    driver.get('https://thejoyfuljourneyer1.blog/')
+
+    print(f"driver title : \n{driver.title}\n")
+
+    # to get data from web page , give to selenium to wait for the page be loaded !!
+    # try:
+    #     elements = WebDriverWait(driver, 20).until(
+    #         EC.presence_of_element_located(
+    #             (By.XPATH, "/html/body/div[1]/div[2]/div/div/div/div[1]/div")
+    #         )
+    #     )
+
+    #     # with xpath of element will return the all data !
+    #     # links = elements.find_elements_by_xpath('//*[@id="post-898"]/div/div[2]/h2/a')
+
+    #     entires = elements.find_elements_by_class_name('entry-title')
+
+    #     for i in entires:
+    #         print(f"blog entire ::  {i.text}")
+    #         print(f"\t===>  : {i.get_attribute('xpath')}")
+
+    # finally:
+    #     # if cant find till 10 sec of any other problent just quite
+    #     driver.quit()
+
+    """
+    <h2 class="card-title entry-title">
+    <a href="https://thejoyfuljourneyer1.blog/reflection-or-real/" title="Reflection? Or real?" rel="bookmark">Reflection? Or real?</a>
+    </h2>
+    """
+    # web_element = driver.find_element_by_xpath("//*[text()='Reflection? Or real?']")
+    web_element = driver.find_element_by_xpath(
+        "//*[contains(text(),'Reflection')]")
+    # Xpath=//tagname[@attribute='value']  <--
+    print(web_element.text)
+    print(web_element.get_attribute('href'))
+
+    driver.quit()
+
+
 if __name__ == '__main__':
-    mac_tes()
+    linux_tes()
