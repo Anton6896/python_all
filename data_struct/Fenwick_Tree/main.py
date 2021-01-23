94d671b80b690f4e358b8967e9917bc6c35b76a2
Ant <fedora@mail.com>
2021-01-23 16:38:23 +0500

    ff

--- data_struct/Fenwick_Tree/main.py
+++ data_struct/Fenwick_Tree/main.py
@@ -1,4 +1,19 @@
 """
 binary index tree
 """
+from copy import deepcopy
+import gmpy
 
+
+def lsb(x):
+    """
+    Returns the index, counting from 0, of the
+    least significant set bit in `x`.
+    """
+    print(bin(x))
+    print((x & -x).bit_length() - 1)
+    return (x & -x).bit_length() - 1
+
+
+if __name__ == '__main__':
+    lsb(10)
