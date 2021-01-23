37773c4660df5bcf95b86a29f4520e05aa0f02a7
Ant <fedora@mail.com>
2021-01-23 11:07:48 +0500

    table in process

--- data_struct/Hashin/from_u/seperate_chain.py
+++ data_struct/Hashin/from_u/seperate_chain.py
@@ -9,11 +9,39 @@ class Entry:
         self.value = value
         self.hash = hash(key)  # <- gey key hash
 
-    def equals(self, other: Entry):
+    # objects with different keys will have diferent hashes
+    # in case of collision check the keys
+    def equals(self, other: Entry) -> bool:
         if self.hash != other.hash:
             return False
 
-        return (self.hash == other.key)
+        return (self.key == other.key)
 
     def __str__(self):
         return "entry : " + str(self.value)
+
+
+class HashTable_my:
+
+    def __init__(self, capasity_=3, load_factor_=0.75):
+        self.capasity = capasity_
+        self.load_factor = load_factor_
+        self.table_list = [None for x in range(capacipy_)]  # fill all with None value 
+        self.threshold = (capasity_ * load_factor_)
+        self.size = 0
+
+    def size(self) -> int:
+        return self.size()
+
+    def is_empty(self) -> bool:
+        return self.size() == 0
+
+    def normalize_index(self, key_hash) -> int:
+        # get index that in range of max capasity
+        return abs(key_hash) % self.capasity
+
+    def clear(self):
+        self.table_list = [None for x in range(self.capasity)]
+        self.size = 0
+
+
