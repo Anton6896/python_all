e28d852a56de56d86e4b0c7b1bb6524ea74aef8e
Ant <fedora@mail.com>
2021-01-25 18:40:12 +0500

    fenwick tree

--- data_struct/Fenwick_Tree/main.py
+++ data_struct/Fenwick_Tree/main.py
@@ -9,14 +9,14 @@ def lsb(x):
     Returns the index, counting from 0, of the
     least significant set bit in `x`.
     """
+
     idx = (x & -x).bit_length() - 1
     arr = [1, 2, 4, 8, 16, 32]
-    print(f"bin(x)  -> {bin(x)}")
-    print(f"bin(-x) -> {bin(-x)}")
-    print(f"bin(x & -x) -> {bin(x & -x)}")  # <- last set bit
-    print(f"lowest value in binary : {arr[idx]}")
-    print(f"bit length of (x & -x) -1  -> {idx}")
-    return (x & -x).bit_length() - 1
+    # print(f"bin(x)  -> {bin(x)}")
+    # print(f"bin(-x) -> {bin(-x)}")
+    # print(f"bin(x & -x) -> {bin(x & -x)}")  # <- last set bit
+    print(f"lowest value in binary for {x} :-> {arr[idx]}")
+    return arr[idx]
 
 
 if __name__ == '__main__':
