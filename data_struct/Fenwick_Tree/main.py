e2e10f78e67f7b5b5fd5dd842c10619e3fa4b654
Ant <fedora@mail.com>
2021-01-25 17:15:54 +0500

    fenwick tree

--- data_struct/Fenwick_Tree/main.py
+++ data_struct/Fenwick_Tree/main.py
@@ -1,5 +1,5 @@
 """
-binary index tree
+Binary Indexed Tree or Fenwick Tree
 """
 from copy import deepcopy
 import gmpy
@@ -10,10 +10,12 @@ def lsb(x):
     Returns the index, counting from 0, of the
     least significant set bit in `x`.
     """
-    print(bin(x))
-    print((x & -x).bit_length() - 1)
+    print(f"bin(x)  -> {bin(x)}")
+    print(f"bin(-x) -> {bin(-x)}")
+    print(f"bin(x & -x) -> {bin(x & -x)}")  # <- last set bit
+    print(f"bit length of (x & -x) -1  -> {(x & -x).bit_length() - 1}")
     return (x & -x).bit_length() - 1
 
 
 if __name__ == '__main__':
-    lsb(10)
+    lsb(10)  # <- index of lowest element
