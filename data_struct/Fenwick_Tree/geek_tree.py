d1ad75a30fcc388093b0f8e89dceb8066a92174d
Ant_win <anton-r@live.com>
2021-01-27 10:55:51 +0500

    working with strings

--- data_struct/Fenwick_Tree/geek_tree.py
+++ data_struct/Fenwick_Tree/geek_tree.py
@@ -54,13 +54,20 @@ def construct(arr, n):
 
 
 if __name__ == '__main__':
-    # Driver code to test above methods
-    freq = [2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9]
-    BITTree = construct(freq, len(freq))
-    print("Sum of elements in arr[0..5] is " + str(getsum(BITTree, 5)))
-    freq[3] += 6
-    updatebit(BITTree, len(freq), 3, 6)
-    print("Sum of elements in arr[0..5]" +
-          " after update is " + str(getsum(BITTree, 5)))
+    # # Driver code to test above methods
+    # freq = [2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9]
+    # BITTree = construct(freq, len(freq))
+    # print("Sum of elements in arr[0..5] is " + str(getsum(BITTree, 5)))
+    # freq[3] += 6
+    # updatebit(BITTree, len(freq), 3, 6)
+    # print("Sum of elements in arr[0..5]" +
+    #       " after update is " + str(getsum(BITTree, 5)))
+    #
+    # # This code is contributed by Raju Varshney
 
-    # This code is contributed by Raju Varshney
+    def tt():
+        # raise NotImplementedError()
+        ...
+
+
+    print(tt())
---
+++ data_struct/Suffix_Preffix/sp.py
@@ -0,0 +1,9 @@
+"""
+LSP array
+Longest Common Prefix (LCP) array
+"""
+
+
+
+if __name__ == '__main__':
+    ...
