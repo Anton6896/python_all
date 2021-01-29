f9218a792d0975a881ee6ef01807f182968de45b
Ant_win <anton-r@live.com>
2021-01-29 10:57:17 +0500

    avl

--- data_struct/Avl_tree/Tree.py
+++ data_struct/Avl_tree/Tree.py
@@ -1,7 +1,8 @@
 class Node:
     def __init__(self, val):
         self.value = val
-        self.left = self.right = None
+        self.left = None
+        self.right = None
         self.height = 1
 
 
@@ -28,11 +29,11 @@ class Tree:
             return self.right_rotation(root)
         """
                  z                                      y 
-                / \                                   /   \
+                / \                                   /   
                y   T4      Right Rotate (z)          x      z
               / \          - - - - - - - - ->      /  \    /  \ 
              x   T3                               T1  T2  T3  T4
-            / \
+            / 
           T1   T2
         """
 
@@ -45,9 +46,9 @@ class Tree:
              z                               z                           x
             / \                            /   \                        /  \ 
            y   T4  Left Rotate (y)        x    T4  Right Rotate(z)    y      z
-          / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / \
+          / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / 
         T1   x                          y    T3                    T1  T2 T3  T4
-            / \                        / \
+            / \                        / 
           T2   T3                    T1   T2
         
         """
@@ -60,9 +61,9 @@ class Tree:
            z                                y
          /  \                            /   \ 
         T1   y     Left Rotate(z)       z      x
-            /  \   - - - - - - - ->    / \    / \
+            /  \   - - - - - - - ->    / \    /  \
            T2   x                     T1  T2 T3  T4
-               / \
+               /  \
              T3  T4
         """
 
@@ -75,17 +76,62 @@ class Tree:
            z                            z                            x
           / \                          / \                          /  \ 
         T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
-            / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    / \
+            / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    /  \
            x   T4                      T2   y                  T1  T2  T3  T4
           / \                              /  \
         T2   T3                           T3   T4
         """
 
-    def right_rotation(self, root) -> Node:
+        return root  # <- get the root after all
+
+    def pop(self, root, value):
         ...
 
-    def left_rotation(self, root) -> Node:
-        ...
+    def right_rotation(self, z: Node) -> Node:
+        """
+                 z                                      y
+                / \                                   /   \
+               y   T4      Right Rotate (z)          x      z
+              / \          - - - - - - - - ->      /  \    /  \
+             x   T3                               T1  T2  T3  T4
+            / \
+          T1   T2
+        """
+        y = z.left
+        t3 = y.right
+
+        # rotate
+        y.right = z
+        z.left = t3
+
+        # update height
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+
+        return y  # <- new root
+
+    def left_rotation(self, z: Node) -> Node:
+        """
+           z                                y
+         /  \                            /   \
+        T1   y     Left Rotate(z)       z      x
+            /  \   - - - - - - - ->    / \    / \
+           T2   x                     T1  T2 T3  T4
+               / \
+             T3  T4
+        """
+        y = z.right
+        t2 = y.left
+
+        # rotation
+        y.left = z
+        z.right = t2
+
+        # height update
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+
+        return y  # <- root now
 
     def get_height(self, node: Node) -> int:
         if not node:
@@ -97,5 +143,38 @@ class Tree:
             return 0
         return self.get_height(node.left) - self.get_height(node.right)
 
-    def pre_order(self, root: Node):
-        pass
+    def pre_order(self, node):
+        if not node:
+            return
+
+        print(f"{node.value} -> ", end=" ")
+        self.pre_order(node.left)
+        self.pre_order(node.right)
+
+    def in_order(self, root):
+        # Left -> Root -> Right
+        res = []
+        if root:
+            res = self.in_order(root.left)
+            res.append(root.value)
+            res = res + self.in_order(root.right)
+        return res
+
+
+if __name__ == '__main__':
+    print("avl tree:")
+    tree = Tree()
+    root = None
+
+    root = tree.push(root, 10)
+    root = tree.push(root, 20)
+    root = tree.push(root, 30)
+    root = tree.push(root, 40)
+    root = tree.push(root, 50)
+    root = tree.push(root, 25)
+
+    print("insertion :: ")
+    print(f"pre order -> {tree.pre_order(root)}")
+    print(f"in order -> {tree.in_order(root)}")
+
+    print("deletion :: ")
