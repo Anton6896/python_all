2befc9fd62545eb0a68954e849f0cf9fd0b1caf0
Mac <Anton-r@live.com>
2021-02-09 08:46:20 +0500

    xxx

--- python_adv/swich.py
+++ python_adv/swich.py
@@ -15,10 +15,11 @@ def switch_good(operation, x, y):
         'add': lambda: x + y,
         'sub': lambda: x - y,
 
-        
+
     }.get(operation, lambda: 'no option')()  # <- all as a function
 
 
 if '__main__' == __name__:
     print(switch_regular('add', 2, 3))
-    print(switch_good('mul', 2, 3))
+    print(switch_good('add', 2, 3))
+
