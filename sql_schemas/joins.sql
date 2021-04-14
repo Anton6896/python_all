ff75042c201a63af4a79616645fbe363df3479db
AntLinux <anton-r@live.com>
2021-04-14 08:37:28 +0300

    dasdsda

---
+++ sql_schemas/joins.sql
@@ -0,0 +1,66 @@
+# find all managers for branches  (inner join)
+select emp_firstname, emp_lastname, emp_salary, branch_name
+from employee
+         join branch on emp_id = mgr_id;
+
+# left / right join ( define an basis for the joining , other missing data fill to null )
+select emp_firstname, emp_lastname, emp_salary, branch_name
+from employee
+         left join branch on emp_id = mgr_id;
+
+select emp_firstname, emp_lastname, emp_salary, branch_name
+from employee
+         right join branch on emp_id = mgr_id;
+
+
+# got name off all employees if they sold at least 30.000
+select distinct emp_firstname
+from employee
+         join works_with on employee.emp_id = works_with.emp_id
+where total_sales > 30000
+   or total_sales = 30000;
+
+select *
+from works_with;
+
+select *
+from employee;
+
+select distinct emp_firstname, emp_lastname
+from employee
+where emp_id in
+      (
+          select emp_id
+          from works_with
+          where total_sales > 30000
+             or total_sales = 30000
+      );
+
+# select all clients who are handled by branch that Michael Scott manager
+select *
+from client
+where branch_id in (
+    select branch_id
+    from branch
+    where mgr_id in
+          (
+              select emp_id
+              from employee
+              where emp_firstname = 'Michael'
+                and emp_lastname = 'Scott'
+          )
+#     one branch is enough for this query
+    limit 1
+);
+
+select client_name
+from client
+         join branch on branch.branch_id = client.branch_id
+         join employee on emp_id = branch.mgr_id
+where emp_firstname = 'Michael'
+  and emp_lastname = 'Scott';
+
+
+
+
+
--- sql_schemas/unions.sql
+++ sql_schemas/unions.sql
@@ -3,3 +3,14 @@ from employee
 union
 select branch_name
 from branch;
+
+# what is company profit  -270500
+select sum(total_sales) as in_and_out
+from works_with
+union
+select sum(emp_salary) as b
+from employee;
+
+select (sum(total_sales) - sum(emp_salary)) as total
+from employee, works_with;
+
