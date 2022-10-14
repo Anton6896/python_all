626e5b6fe967139068a12594406fb569cce9fe3f
AntonR <anton-r@live.com>
2022-10-14 09:50:37 +0300

    update

--- programming_patterns/interpriter/lexer.py
+++ programming_patterns/interpriter/lexer.py
@@ -1,4 +1,3 @@
-from curses.ascii import isdigit
 from enum import Enum
 
 
@@ -7,57 +6,118 @@ class Token:
         INTEGER = 0
         PLUS = 1
         MINUS = 2
-        L_PAREN = 3
-        R_PAREN = 4
-        SPACE = 5
+        LPAREN = 3
+        RPAREN = 4
 
-    def __init__(self, type: Type, text: str) -> None:
+    def __init__(self, type, text):
         self.type = type
         self.text = text
 
-    def __str__(self) -> str:
-        return f'{self.text}'
+    def __str__(self):
+        return f'`{self.text}`'
 
 
-def lex(input: str) -> list[Token]:
-    # (1+2) - 3 check this kind of expressions
+def lex(input):
+    result = []
 
-    acc: list[Token] = []
     i = 0
-
     while i < len(input):
         if input[i] == '+':
-            acc.append(Token(Token.Type.PLUS, input[i]))
+            result.append(Token(Token.Type.PLUS, '+'))
         elif input[i] == '-':
-            acc.append(Token(Token.Type.MINUS, input[i]))
-        elif input[i] == ')':
-            acc.append(Token(Token.Type.R_PAREN, input[i]))
+            result.append(Token(Token.Type.MINUS, '-'))
         elif input[i] == '(':
-            acc.append(Token(Token.Type.L_PAREN, input[i]))
-        elif input[i] == ' ':
-            acc.append(Token(Token.Type.SPACE, input[i]))
-
-        # check numbers
-        else:
-            digits = [input[i], ]
+            result.append(Token(Token.Type.LPAREN, '('))
+        elif input[i] == ')':
+            result.append(Token(Token.Type.RPAREN, ')'))
+        else:  # must be a number
+            digits = [input[i]]
             for j in range(i + 1, len(input)):
                 if input[j].isdigit():
                     digits.append(input[j])
                     i += 1
                 else:
+                    result.append(Token(Token.Type.INTEGER,
+                                        ''.join(digits)))
                     break
-            acc.append(Token(Token.Type.INTEGER, ''.join(digits)))
-
         i += 1
 
-    return acc
+    return result
 
 
-def calc(input):
+# ↑↑↑ lexing ↑↑↑
+
+# ↓↓↓ parsing ↓↓↓
+
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+
+class BinaryOperation:
+    class Type(Enum):
+        ADDITION = 0
+        SUBTRACTION = 1
+
+    def __init__(self):
+        self.type = None
+        self.left = None
+        self.right = None
+
+    @property
+    def value(self):
+        if self.type == self.Type.ADDITION:
+            return self.left.value + self.right.value
+        elif self.type == self.Type.SUBTRACTION:
+            return self.left.value - self.right.value
+
+
+def parse(tokens):
+    result = BinaryOperation()
+    have_lhs = False
+    i = 0
+    while i < len(tokens):
+        token = tokens[i]
+
+        if token.type == Token.Type.INTEGER:
+            integer = Integer(int(token.text))
+            if not have_lhs:
+                result.left = integer
+                have_lhs = True
+            else:
+                result.right = integer
+        elif token.type == Token.Type.PLUS:
+            result.type = BinaryOperation.Type.ADDITION
+        elif token.type == Token.Type.MINUS:
+            result.type = BinaryOperation.Type.SUBTRACTION
+        elif token.type == Token.Type.LPAREN:  # note: no if for RPAREN
+            j = i
+            while j < len(tokens):
+                if tokens[j].type == Token.Type.RPAREN:
+                    break
+                j += 1
+            # preprocess subexpression
+            subexpression = tokens[i + 1:j]
+            element = parse(subexpression)
+            if not have_lhs:
+                result.left = element
+                have_lhs = True
+            else:
+                result.right = element
+            i = j  # advance
+        i += 1
+    return result
+
+def eval(input):
     tokens = lex(input)
-    print(''.join(map(str, tokens)))
+    print(' '.join(map(str, tokens)))
 
+    parsed = parse(tokens)
+    print(f'{input} = {parsed.value}')
 
 if __name__ == '__main__':
-    exp = '(1+2) - 3'
-    calc(exp)
+    eval('(13+4)-(12+1)')
+    eval('1+(3-4)')
+
+    # this won't work
+    eval('1+2+(3-4)')
