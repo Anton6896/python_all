d93675bd55523dffd5657bd4a926c6fae4a94ab9
anton-r <anton-r@live.com>
2023-04-22 14:27:06 +0300

    updated

---
+++ python_adv/chat-v2/main.py
@@ -0,0 +1,12 @@
+import threading
+import socket
+
+"""
+https://www.youtube.com/watch?v=3UOyky9sEQY&list=WL&index=2&ab_channel=NeuralNine
+"""
+
+HOST = '127.0.0.1'
+PORT = 9009
+
+if __name__ == '__main__':
+    print('data')
