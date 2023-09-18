009434d29a971baa9ad6b9f5daf1346e52798dd0
Anton.R <Anton-r@live.com>
2023-09-18 19:42:23 +0300

    fixin status

--- python_adv/calculator/main.py
+++ python_adv/calculator/main.py
@@ -96,34 +96,40 @@ class Node:
             return f'{self.token.value}({self.weight})'
         return ''
 
+    def __repr__(self):
+        if self.token:
+            return f'<Node> {self.token.value}[{self.weight}]'
+        return ''
+
     def calculate(self):
+        if self.token.type_of != Token.ACTION:
+            return
+
         if (
-            self.token.type_of == Token.ACTION
-            and self.left.value
-            and self.right.value
+                self.left.value and self.right.value
         ):
             self.value = self.token.use_it()(self.left.value, self.right.value)
 
         elif (
-            self.token.type_of == Token.ACTION
-            and self.left.value
-            and self.right.token.type_of == Token.NUMBER
+                self.left.value and self.right.token.type_of == Token.NUMBER
         ):
             self.value = self.token.use_it()(self.left.value, self.right.token.value)
 
         elif (
-            self.token.type_of == Token.ACTION
-            and self.left.token.type_of == Token.NUMBER
-            and self.right.value
+                self.left.token.type_of == Token.NUMBER and self.right.value
         ):
             self.value = self.token.use_it()(self.left.token.value, self.right.value)
 
         elif (
-            self.token.type_of == Token.ACTION
-            and self.left.token.type_of == Token.NUMBER
-            and self.right.token.type_of == Token.NUMBER
+                self.left.token.type_of == Token.NUMBER and self.right.token.type_of == Token.NUMBER
         ):
             self.value = self.token.use_it()(self.left.token.value, self.right.token.value)
+
+        # elif (
+        #     self.left.token.type_of == Token.ACTION
+        # ):
+        #     self.left.value = self.left.calculate()
+        return self.value
 
     @property
     def weight(self):
@@ -140,38 +146,46 @@ class Node:
 class Tree:
     def __init__(self):
         self.root: Node | None = None
+        self.response = []
 
     def push(self, new_node: Node):
         if not self.root:
-            logger.info('pushing to root')
+            logger.info('%s pushing to root', new_node)
             self.root = new_node
             return self
 
         current_node: Node = self.get_most_right(self.root)
         self._push(current_node, new_node)
 
+    """
+    this logic not working ! 
+    will not compare weight. now will check instance type 
+    """
+
     def _push(self, current_node: Node, new_node: Node):
-        if (
-                not current_node.parent
-                and not current_node.left
-                and current_node.weight > new_node.weight
-        ):
-            logger.info('pushing to left (from root)')
+        # working with root
+        if all([
+            not current_node.parent,
+            not current_node.left
+        ]):
+            logger.info('new root %s, left %s', new_node, current_node)
             current_node.parent = new_node
             new_node.left = current_node
             self.root = new_node
             return self
 
-        if (
-                not current_node.parent
-                and current_node.left
-                and new_node.weight > current_node.weight
-        ):
-            logger.info('pushing to right (from root)')
+        if all([
+            not current_node.parent,
+            current_node.left,
+            not current_node.right
+        ]):
+            logger.info('%s pushing to right', new_node)
+            new_node.left = current_node.right
             current_node.right = new_node
             new_node.parent = current_node
             return self
 
+        # other movements
         if current_node.weight > new_node.weight:
             self.lighter_weight(current_node, new_node)
         elif current_node.weight < new_node.weight:
@@ -180,50 +194,61 @@ class Tree:
             self.same_weight(current_node, new_node)
 
     def lighter_weight(self, current_node: Node, new_node: Node):
-        logger.info(f'current %s heavier weight then new node %s', current_node.weight, new_node.weight)
+        logger.info('new node %s is lighter then %s', new_node, current_node)
+
+        if not current_node.parent:
+            logger.info('new root %s, left %s', new_node, current_node)
+            new_node.left = current_node
+            current_node.parent = new_node
+            self.root = new_node
+            return self
 
         self._push(current_node.parent, new_node)
 
     def heavier_weight(self, current_node: Node, new_node: Node):
-        logger.info(f'current %s lighter weight then new node %s', current_node.weight, new_node.weight)
+        logger.info('new node %s is havier then %s', new_node, current_node)
         if (
-            current_node.left
-            and not current_node.right
+                current_node.left
+                and not current_node.right
         ):
             current_node.right = new_node
             new_node.parent = current_node
             return self
 
         # * | /
-        if (
-            new_node.weight > current_node.weight
-            and new_node.weight < current_node.right.weight
-        ):
+        if current_node.weight < new_node.weight < current_node.right.weight:
             new_node.left = current_node
             new_node.parent = current_node.parent
             new_node.parent.right = new_node
             current_node.parent = new_node
 
     def same_weight(self, current_node: Node, new_node: Node):
-        logger.info(f'current %s same weight then new node %s', current_node.weight, new_node.weight)
-        if not current_node.left:
-            raise RuntimeError('invalid operations (++, -- ...)')
+        logger.info(f'current %s same weight then new node %s', current_node, new_node)
+        # if not current_node.left:
+        #     raise RuntimeError('invalid operations (++, -- ...)')
 
-        new_node.left = current_node.right
-        current_node.right.parent = new_node
-        current_node.right = new_node
-        new_node.parent = current_node
+        if not current_node.parent:
+            self.root = new_node
+        else:
+            new_node.parent = current_node.parent
+
+        new_node.left = current_node
+        current_node.parent = new_node
+
         return self
 
-    def calculate_self(self, node: Node) -> float:
-        # Left -> Root -> Right
-        res = []
-        if node:
-            node.calculate()
-            res.extend(self.calculate_self(node.left))
-            if node.value:
-                res.append(node.value)
-            res.extend(self.calculate_self(node.right))
+    def calculate_self(self, node: Node | None, res=None) -> float | list:
+        print(node.left, node, node.right)
+        if not node or node.token.type_of != Token.ACTION:
+            return []
+
+        if node.right:
+            res.extend(self.calculate_self(node.right, res))
+        if node.left:
+            res.extend(self.calculate_self(node.left, res))
+
+        node.calculate()
+        res.append(node.calculate())
 
         return res
 
@@ -236,12 +261,13 @@ class Tree:
         # Left -> Root -> Right
         res = []
         if node:
-            if node.left:
-                res.extend(self.in_order(node.left))
+            res.extend(self.in_order(node.left))
             res.append(node.token.value)
-            if node.right:
-                res.extend(self.in_order(node.right))
+            res.extend(self.in_order(node.right))
         return res
+
+    def __repr__(self) -> str:
+        return f'<Tree> {self.in_order(self.root)}'
 
 
 class Worker:
@@ -273,10 +299,11 @@ class Worker:
 
             main_tree.push(Node(token=token))
 
-        logger.info('inorder %s', main_tree.in_order(main_tree.root))
-        return main_tree.calculate_self(main_tree.root)
+        logger.info('inorder %s', main_tree)
+        res = []
+        return main_tree.calculate_self(main_tree.root, res)
 
 
 if __name__ == '__main__':
-    result = Worker('1 + 2 -1 + 5').run()
+    result = Worker('2 + 3 - 1 + 1 + 4 -2').run()
     logger.info('result: %s', result)
